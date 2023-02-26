c0cbed7a52887ad92eb49ae49035785e1a69bb5e
Artem Farafonov <artem_farafonov@epam.com>
2023-02-26 21:49:59 +0300

    task-3: implement cors

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -17,6 +17,18 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
+    httpApi: {
+      cors: {
+        allowedOrigins: [
+          "http://localhost:5555",
+          "http://db27n60o60d5z.cloudfront.net",
+          "https://db27n60o60d5z.cloudfront.net",
+        ],
+        allowedMethods: ["GET", "POST"],
+        allowCredentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+      },
+    },
   },
   // import the function via paths
   functions: { getProductsList, getProductsById },
