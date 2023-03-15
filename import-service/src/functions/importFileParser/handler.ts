8f35166d210c885dbfd29ea2d964734ec4aa27c3
Artem Farafonov <artem_farafonov@epam.com>
2023-03-15 04:34:25 +0300

    small fix

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -12,34 +12,29 @@ import stripBom from "strip-bom-stream";
 import { Readable } from "node:stream";
 import middy from "@middy/core";
 import ssm from "@middy/ssm";
-import { SendMessageBatchCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 const client = new S3Client({});
 const sqsClient = new SQSClient({});
 
-export const sendToQueue = async (queueUrl: string, record: object[]) => {
-  const entries = record.map((record, index) => ({
-    Id: String(index),
-    MessageBody: JSON.stringify(record),
-  }));
-  const chunkSize = 10;
-  for (let i = 0; i < entries.length; i += chunkSize) {
-    const chunk = entries.slice(i, i + chunkSize);
-    const sendResult = await sqsClient.send(
-      new SendMessageBatchCommand({
-        QueueUrl: queueUrl,
-        Entries: chunk,
-      })
-    );
-    console.log("Messages send result", sendResult);
-  }
+export const sendToQueue = async (queueUrl: string, record: object) => {
+  const sendResult = await sqsClient.send(
+    new SendMessageCommand({
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify(record),
+    })
+  );
+  console.log("Messages send result", sendResult);
 };
 
-export const processFile = async (key: string, bucket: string) => {
+export const processFile = async (
+  key: string,
+  bucket: string,
+  queueUrl: string
+) => {
   const read = new GetObjectCommand({
     Key: key,
     Bucket: bucket,
   });
-  const records = [];
   const response = await client.send(read);
   const pipe = (response.Body as Readable)
     .pipe(stripBom())
@@ -50,13 +45,12 @@ export const processFile = async (key: string, bucket: string) => {
     )
     .on("data", (data) => {
       console.log(data);
-      records.push(data);
+      sendToQueue(queueUrl, data);
     });
   await new Promise((resolve, reject) => {
     pipe.on("end", resolve);
     pipe.on("error", reject);
   });
-  return records;
 };
 export const moveToProcessed = async (key: string, bucket: string) => {
   const copyInput: CopyObjectCommandInput = {
@@ -71,11 +65,11 @@ export const moveToProcessed = async (key: string, bucket: string) => {
 export const importProductsParser = async (event: S3Event, context) => {
   await Promise.all(
     event.Records.map(async (record) => {
-      const records = await processFile(
+      await processFile(
         record.s3.object.key,
-        record.s3.bucket.name
+        record.s3.bucket.name,
+        context.queueUrl
       );
-      await sendToQueue(context.queueUrl, records);
       await moveToProcessed(record.s3.object.key, record.s3.bucket.name);
     })
   );
