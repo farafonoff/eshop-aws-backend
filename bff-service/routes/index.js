bc8574e54bd01d9ad50b6b95d80e6447b5e8eaaf
Artem Farafonov <artem_farafonov@epam.com>
2023-04-04 14:30:58 +0300

    dotenv & debug

--- bff-service/routes/index.js
+++ bff-service/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+    cartApi: process.env.cart_api,
+    productApi: process.env.product_api,
+  });
 });
 
 module.exports = router;
